5e439a15173c865ed5e28f0a12a34aad4a1fc15d
vickywinkel <152075054+vickywinkel@users.noreply.github.com>
2024-05-27 14:03:45 -0300

    ContraseñaBien
    al precionar el boton va a corroborar si esta bien la contraseña

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public string contrasena = "hola";
+    public InputField contra; 
+    public void BotonAceptar()
+    {
+        if (contra.text == contrasena)
+        {
+            Debug.Log("Access granted");
+        }
+        else
+        {
+            Debug.Log("Access denied");
+        }
+    }
+}
